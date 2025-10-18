2c7832ee36f976dcb8750ea4351009c77ae9c513
MartinC <cabreramartin362@gmail.com>
2025-10-18 01:30:57 -0600

    solucion error crear

--- controllers/NewController.js
+++ controllers/NewController.js
@@ -80,7 +80,7 @@ const create = (request, response) => {
     )
         .catch(err => {
             console.log(err)
-            response.status(500).send('Error al crear');
+            response.status(500).json({ message: 'Error al crear la noticia', error: err });
         })
 }
 
